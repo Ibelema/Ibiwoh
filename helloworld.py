8c147c6286f384fd93a592f8b3acbef9f2b6af0f
Ibelema <ibelemaiwoh02@gmail.com>
2020-03-14 11:58:00 +0100

    Sample

---
+++ helloworld.py
@@ -0,0 +1,2 @@
+print("Hello World, Its David!!!!")
+print("Lol")
---
+++ vowel.py
@@ -0,0 +1,11 @@
+vowel = ["a","e","i","o","u"]
+vowel_count = 0
+
+user = input("Enter a spelling: ")
+user = user.lower()
+
+for i in vowel:
+    if i in user:
+        vowel_count +=1
+
+print("Number of vowel is {}".format(vowel_count))
